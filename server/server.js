37927eb91c4dbdb6225139c619b4b97f41d2aa42
Colin Z. Robertson <czr@rtnl.org.uk>
2018-12-04 15:54:10 +0000

    Remove example endpoints

--- server/server.js
+++ server/server.js
@@ -10,14 +10,5 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/life-progress.json', (req, res) => res.json({
   "life-progress": getLifeProgress(),
 }));
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
