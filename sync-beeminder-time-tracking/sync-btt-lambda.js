0d8eb9f033a1454c845034c9402abd70f38e6304
Colin Z. Robertson <czr@rtnl.org.uk>
2019-02-22 10:33:52 +0000

    Add AWS Lambda handler

---
+++ sync-beeminder-time-tracking/sync-btt-lambda.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const minimist = require('minimist')
+const moment = require('moment')
+
+const beeminder = require('./beeminder')
+const bttSync = require('./beeminder-time-tracking-sync')
+const tt = require('time-tracking')
+
+var beeminder_username = process.env.BEEMINDER_USERNAME
+var beeminder_auth_token = process.env.BEEMINDER_AUTH_TOKEN
+var beeminder_goal = process.env.BEEMINDER_GOAL
+
+var goal = beeminder.goal(
+  beeminder_username,
+  beeminder_auth_token,
+  beeminder_goal,
+)
+
+async function handler(event) {
+  var apply = false
+  var since = moment().subtract(7, 'days').startOf('day')
+
+  var tag = beeminder_goal
+
+  var iCalStr = await tt.getICalStr()
+
+  var events = bttSync.sortEvents(tt.taggedEvents(since, iCalStr)[tag] || [])
+  
+  var datapoints = await goal.datapoints()
+  datapoints = bttSync.sortAndFilterDatapoints(datapoints, since)
+
+  var actions = bttSync.calcSyncActions(events, datapoints)
+
+  console.log(actions)
+
+  if (apply) {
+    bttSync.applyActions(actions, goal)
+  }
+}
+
+module.exports = { handler };
