2ed09fc0658478d698e9a6e0c674e6eff62e4a99
Colin Z. Robertson <czr@rtnl.org.uk>
2019-03-20 20:34:46 +0000

    Display health log attributes as select elements and send to server on change

--- client/src/HealthLog.js
+++ client/src/HealthLog.js
@@ -7,7 +7,24 @@ var fetchJson = async (url) => {
   const response = await fetch(url)
   const body = await response.json()
   if (response.status !== 200) throw Error(body.message)
-  return body;
+  return body
+}
+
+var putJson = async (url, data) => {
+  const response = await fetch(
+    url,
+    {
+      method: 'PUT',
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+    }
+  )
+  if (response.status >= 300) {
+    const body = await response.json()
+    throw Error(body.message)
+  }
 }
 
 class HealthLog extends React.Component {
@@ -26,19 +43,45 @@ class HealthLog extends React.Component {
       .catch(err => console.log(err))
   }
 
+  currentDate = () => {
+    return moment().format('YYYY-MM-DD')
+  }
+
   callHealthLog = async () => {
-    return fetchJson('/api/health-log/days/2019-03-20')
+    let date = this.currentDate()
+    return fetchJson(`/api/health-log/days/${date}`)
   }
 
   callSchema = async () => {
     return fetchJson('/api/health-log/schema')
   }
 
+  attributeValues(attribute) {
+    if (this.state.schema[attribute]) {
+      return this.state.schema[attribute]
+    }
+    return []
+  }
+
   formatValue(attribute, value) {
     if (this.state.schema[attribute]) {
-      return this.state.schema[attribute][value]
+      return this.state.schema[attribute][value - 1]
     }
     return value
+  }
+
+  handleAttributeChange(event) {
+    let attribute = event.target.name
+    let value = Number(event.target.value)
+    let attributes = this.state.attributes
+
+    this.setState(
+      { attributes: { ...attributes, [attribute]: value } },
+      () => {
+        let date = this.currentDate()
+        putJson(`/api/health-log/days/${date}`, this.state.attributes)
+      }
+    )
   }
 
   render() {
@@ -48,12 +91,20 @@ class HealthLog extends React.Component {
       <div className="HealthLog">
         <h1>Health</h1>
         <table className="attributes">
-          {Object.keys(attributes).sort().map(attribute =>
-            <tr>
-              <td className="attribute">{attribute}</td>
-              <td className="value">{this.formatValue(attribute, attributes[attribute])}</td>
-            </tr>
-          )}
+          <tbody>
+            {Object.keys(attributes).sort().map(attribute =>
+              <tr key={attribute}>
+                <td className="attribute">{attribute}</td>
+                <td className="value">
+                  <select name={attribute} onChange={this.handleAttributeChange.bind(this)}>
+                    {this.attributeValues(attribute).map((value, index) =>
+                      <option value={index + 1} key={index + 1} selected={index + 1 === attributes[attribute]}>{value}</option>
+                    )}
+                  </select>
+                </td>
+              </tr>
+            )}
+          </tbody>
         </table>
       </div>
     );
