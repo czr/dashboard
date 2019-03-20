e8ab6c76f6f4c828cdb7faf93b16848af29eda3d
Colin Z. Robertson <czr@rtnl.org.uk>
2019-03-20 12:35:40 +0000

    Remove unnecessary semicolons

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
 require('dotenv').config()
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const { getTimeTracking } = require('ical-tagged-time');
-const beeminder = require('beeminder-js');
+const express = require('express')
+const bodyParser = require('body-parser')
+const { getTimeTracking } = require('ical-tagged-time')
+const beeminder = require('beeminder-js')
 
-const getLifeProgress = require('./life-progress');
-const trello = require('./trello');
+const getLifeProgress = require('./life-progress')
+const trello = require('./trello')
 
-const app = express();
-const port = process.env.PORT || 5000;
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const app = express()
+const port = process.env.PORT || 5000
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/api/life-progress', (req, res) => res.json({
   "life-progress": getLifeProgress(),
-}));
+}))
 
 app.get('/api/time-tracking', async (req, res) => {
   res.json(
     await getTimeTracking(process.env.GOOGLE_CALENDAR_URL)
   )
-});
+})
 
 app.get('/api/trello/mit', async (req, res) => {
   res.json(
@@ -32,7 +32,7 @@ app.get('/api/trello/mit', async (req, res) => {
       process.env.TRELLO_TOKEN,
     )
   )
-});
+})
 
 app.post('/api/trello/done', async (req, res) => {
   await trello.moveCard(
@@ -50,6 +50,6 @@ app.post('/api/trello/done', async (req, res) => {
     comment: req.body.card.name,
   })
   res.json({"status": "ok"})
-});
+})
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
