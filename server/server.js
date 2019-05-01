2503940628567459162456802a576c00b34344ba
Colin Z. Robertson <czr@rtnl.org.uk>
2019-05-01 15:13:46 +0100

    Copy client/build into server directory

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static('../client/build'))
+app.use(express.static('client-build'))
 
 app.get('/api/life-progress', (req, res) => res.json({
   "life-progress": getLifeProgress(),
